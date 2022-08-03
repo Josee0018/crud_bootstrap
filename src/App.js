ff80ad9e966a9bade25e84d8bdd0211fed316d43
Josee0018 <jalvarez0018@gmail.com>
2022-08-03 13:17:33 -0500

    components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.scss";
 
 import ButtonB from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import ToggleButton from "react-bootstrap/ToggleButton";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import React, { useState } from "react";
+import Modalinput from "./components/Modalinput";
+import ModalDelete from "./components/ModalDelete";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
@@ -64,7 +62,7 @@ const App = () => {
   const handleModalDelete = (data) => {
     setShowModalDelete(!showModalDelete);
     setPersonSelect(data.id);
-    setPersonSelectName(data.name)
+    setPersonSelectName(data.name);
   };
   const handleModalEdit = (type, data) => {
     setShowModal(!showModal);
@@ -136,120 +134,24 @@ const App = () => {
         >
           <div>Add Person</div>
         </ButtonB>
-        <Modal show={showModal} onHide={() => handleModalEdit("reset")}>
-          <Modal.Header closeButton></Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  key="id"
-                  name="id"
-                  hidden
-                  value={input.id}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  key="name"
-                  name="name"
-                  value={input.name}
-                  onChange={handleInputChange}
-                  placeholder='"Juan" , "Maximiliano" , "John"'
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  key="lastname"
-                  name="lastname"
-                  value={input.lastname}
-                  onChange={handleInputChange}
-                  placeholder='"Alvarez" , "Perez" , "Smith"'
-                />
-              </Form.Group>
-              <br></br>
-              <Form.Group>
-                <Form.Label>Gender</Form.Label>
-                <Row xs={6}>
-                  {genders.map((gender, idx) => (
-                    <Col key={idx}>
-                      <ToggleButton
-                        id={`genders-${idx}`}
-                        type="checkbox"
-                        variant={
-                          idx % 2 ? "outline-primary" : "outline-primary"
-                        }
-                        name="gender"
-                        value={gender.value}
-                        checked={input.gender === genders.value}
-                        onChange={handleInputChange}
-                      >
-                        {gender.name}
-                      </ToggleButton>
-                    </Col>
-                  ))}
-                </Row>
-              </Form.Group>
-              <ButtonGroup className="mb-2"></ButtonGroup>
-              <Form.Group>
-                <Form.Label>Address</Form.Label>
-                <Form.Control
-                  type="text"
-                  key="address"
-                  name="address"
-                  value={input.address}
-                  onChange={handleInputChange}
-                  placeholder='"Av. Don Bosco" , "Primero de mayo 3-75 y felipe II" , "Barcelona 51652"'
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Birthday</Form.Label>
-                <Form.Control
-                  type="date"
-                  key="birthday"
-                  name="birthday"
-                  placeholder="Due date"
-                  value={input.birthday}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <ButtonB
-              variant="primary"
-              onClick={isEditable ? editPerson : addPerson}
-            >
-              {isEditable ? "Edit person" : "Add person"}
-            </ButtonB>
-          </Modal.Footer>
-        </Modal>
+        <Modalinput
+          show={showModal}
+          onHide={() => handleModalEdit("reset")}
+          onClick={isEditable ? editPerson : addPerson}
+          handleInputChange={handleInputChange}
+          input={input}
+          genders={genders}
+        >
+          {isEditable ? "Edit person" : "Add person"}
+        </Modalinput>
 
-        <Modal
-          id="modal_delete"
+        <ModalDelete
           show={showModalDelete}
           onHide={() => handleModalDelete()}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title>Delete Person</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Are you sure you want to delete to {personSelectName ? personSelectName : ""}?</Modal.Body>
-          <Modal.Footer>
-            <ButtonB variant="secondary" onClick={() => handleModalDelete()}>
-              Close
-            </ButtonB>
-            <ButtonB variant="primary" onClick={() => deletePerson()}>
-              Confirm
-            </ButtonB>
-          </Modal.Footer>
-        </Modal>
+          personSelectName={personSelectName}
+          onClick={handleModalDelete}
+          onClick2={() => deletePerson()}
+        ></ModalDelete>
 
         <div className="container">
           <Table striped bordered hover>
